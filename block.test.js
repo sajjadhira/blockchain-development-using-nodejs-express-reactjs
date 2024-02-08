d68c8e4b89aaa84caef5aa5c61b72f08cb181a7f
sajjadhira <sajjad.hira12@gmail.com>
2024-02-08 21:32:00 +0600

    Block nonce and difficulty introduced

--- block.test.js
+++ block.test.js
@@ -7,13 +7,24 @@ describe("Block", () => {
   const lastHash = "foo-hash";
   const hash = "bar-hash";
   const data = ["blockchain", "data"];
-  const block = new Block({ timestamp, lastHash, hash, data });
+  const nonce = 1;
+  const difficulty = 1;
+  const block = new Block({
+    timestamp,
+    lastHash,
+    hash,
+    data,
+    nonce,
+    difficulty,
+  });
 
   it("has a timestamp, lastHash, hash, and data property", () => {
     expect(block.timestamp).toEqual(timestamp);
     expect(block.lastHash).toEqual(lastHash);
     expect(block.hash).toEqual(hash);
     expect(block.data).toEqual(data);
+    expect(block.nonce).toEqual(nonce);
+    expect(block.difficulty).toEqual(difficulty);
   });
 
   describe("genesis()", () => {
